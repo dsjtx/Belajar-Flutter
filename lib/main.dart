5663bd8a2d7cfec1b40744d49685b86b5b074478
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-07-14 22:59:31 +0700

    mainAxisAlignment & crossAxisAlignment, Row & Column

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text("Text apaan nih?"),
-            Text("Text apaan nih?"),
-            Text("Text apaan nih?")
+            Text("Text 1 apaan nih?"),
+            Text("Text 2 apaan nih?"),
+            Text("Text 3 apaan nih?"),
+            Row(
+              children: <Widget>[
+                Text("Text 4 apaan nih?"),
+                Text("Text 5 apaan nih?"),
+                Text("Text 6 apaan nih?")
+              ],
+            )
           ],
         ),
       ),
