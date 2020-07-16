93f3dea4d9b3227f907fc6d744b0b51a5a0b7817
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-07-17 01:34:01 +0700

    Latihan AppBar Gradasi Corak

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +6,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.adb, color: Colors.white),
+          title: Text(
+            "AppBar Example",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+            IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {}),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight),
+                image: DecorationImage(
+                    image: AssetImage("assets/corak.png"),
+                    fit: BoxFit.cover,
+                    repeat: ImageRepeat.repeat)),
+          ),
+        ),
+      ),
     );
   }
 }
