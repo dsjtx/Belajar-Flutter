191ac6653de13e6e192da3b5c0ec6431c9c027df
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-07-17 17:00:47 +0700

    Latihan TextField Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,35 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Latihan TextField"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            TextField(
-              // obscureText: true,
-              maxLength: 5,
-              onChanged: (value) {
-                setState(() {});
-              },
-              controller: controller,
-            ),
-            Text(controller.text),
-          ],
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                // obscureText: true,
+                decoration: InputDecoration(
+                  fillColor: Colors.lightBlue[50],
+                  filled: true,
+                  icon: Icon(Icons.adb),
+                  suffix: Container(width: 5, height: 5, color: Colors.red,),
+                  prefixIcon: Icon(Icons.person),
+                  // prefixText: "Name : ",
+                  prefixStyle: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+                  labelText: "Nama Lengkap",
+                  hintText: "Nama Lengkap Anda",
+                  hintStyle: TextStyle(fontSize: 12),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                maxLength: 5,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: controller,
+              ),
+              Text(controller.text),
+            ],
+          ),
         ),
       ),
     );
