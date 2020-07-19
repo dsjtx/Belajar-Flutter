8c3f1fb5a0d98472adc7f025652e379a95d827d8
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-07-19 22:39:53 +0700

    Latihan Appbar with Custom Height

--- lib/main.dart
+++ lib/main.dart
@@ -13,80 +13,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Latihan Hero Animation",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(
-            builder: (context) {
-              return SecondPage();
-            },
-          ));
-        },
-        child: Hero(
-          tag: 'pp',
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: Container(
-              width: 100,
-              height: 100,
-              child: Image(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      "https://esports.id/img/article/16120190729070632.jpg")),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text(
-          "Latihan Hero Animation",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) {
-                return MainPage();
-              },
-            ));
-          },
-          child: Hero(
-            tag: 'pp',
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: AppBar(
+            backgroundColor: Colors.amber,
+            flexibleSpace: Positioned(
+              bottom: 0,
+              right: 0,
               child: Container(
-                width: 200,
-                height: 200,
-                child: Image(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        "https://esports.id/img/article/16120190729070632.jpg")),
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  "Appbar with Custom Height",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
               ),
             ),
           ),
