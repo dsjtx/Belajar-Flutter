7b913324975527b3532bebe9e63c8b3cb7dee799
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-07-19 23:30:29 +0700

    Latihan Ubah Tampilan TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,53 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 // void main() => runApp(MyApp());
-void main() {
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((value) {
-    runApp(MyApp());
-  });
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  TabBar myTabBar = TabBar(
+      indicator: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Colors.blue, 
+            Colors.blueAccent[400]],
+        ),
+        // color: Colors.red
+      ),
+      // indicatorColor: Colors.black,
+      tabs: <Widget>[
+        Tab(
+          icon: Icon(Icons.comment),
+          text: "Comments",
+        ),
+        Tab(
+          icon: Icon(Icons.computer),
+          text: "Computer",
+        )
+      ]);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
-        length: 4,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
-            title: Text("Latihan TabBar"),
-            bottom: TabBar(tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.comment),
-                text: "Comments",
-              ),
-              Tab(
-                child: Image(
-                  image: AssetImage("assets/facebook.png"),
-                ),
-              ),
-              Tab(
-                icon: Icon(Icons.computer),
-              ),
-              Tab(
-                text: "News",
-              )
-            ]),
-          ),
+              title: Text("Latihan Ubah Tampilan TabBar"),
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+                child: Container(
+                    // color: Colors.amber,
+                    child: myTabBar),
+              )),
           body: TabBarView(children: <Widget>[
             Center(
               child: Text("Tab 1"),
             ),
             Center(
               child: Text("Tab 2"),
-            ),
-            Center(
-              child: Text("Tab 3"),
-            ),
-            Center(
-              child: Text("Tab 4"),
             ),
           ]),
         ),
