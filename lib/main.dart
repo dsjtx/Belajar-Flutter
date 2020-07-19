c9b1e6b0348b1982a8d4653a3f067f71a6e60ada
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-07-19 23:43:40 +0700

    Latihan Menampilkan QR Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 // void main() => runApp(MyApp());
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  TabBar myTabBar = TabBar(
-      indicator: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: <Color>[
-            Colors.blue, 
-            Colors.blueAccent[400]],
-        ),
-        // color: Colors.red
-      ),
-      // indicatorColor: Colors.black,
-      tabs: <Widget>[
-        Tab(
-          icon: Icon(Icons.comment),
-          text: "Comments",
-        ),
-        Tab(
-          icon: Icon(Icons.computer),
-          text: "Computer",
-        )
-      ]);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-              title: Text("Latihan Ubah Tampilan TabBar"),
-              bottom: PreferredSize(
-                preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-                child: Container(
-                    // color: Colors.amber,
-                    child: myTabBar),
-              )),
-          body: TabBarView(children: <Widget>[
-            Center(
-              child: Text("Tab 1"),
-            ),
-            Center(
-              child: Text("Tab 2"),
-            ),
-          ]),
+      home: Scaffold(
+        body: Center(
+          child: QrImage(
+            version: 6,
+            // backgroundColor: Colors.grey,
+            foregroundColor: Colors.black,
+            errorCorrectionLevel: QrErrorCorrectLevel.M,
+            padding: EdgeInsets.all(20),
+            size: 300,
+            data: "https://www.instagram.com/daniel_smnjuntak/"),
         ),
       ),
     );
