c092930c980f412c76a045b989a25c390f2f2e5e
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-07-25 02:02:33 +0700

    Latihan Membuat Button Belah Ketupat Warna Warni (Transform) 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/ColorfulButton.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math';
 
 // void main() => runApp(MyApp());
 void main() => runApp(MyApp());
@@ -23,6 +24,123 @@ class MyApp extends StatelessWidget {
               ColorfulButton(Colors.green, Colors.purple, Icons.computer),
               ColorfulButton(Colors.blue, Colors.yellow, Icons.contact_phone),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class ColorfulButton extends StatefulWidget {
+  Color mainColor, secondColor;
+  IconData iconData;
+
+  @override
+  _ColorfulButtonState createState() =>
+      _ColorfulButtonState(mainColor, secondColor, iconData);
+
+  ColorfulButton(this.mainColor, this.secondColor, this.iconData);
+}
+
+class _ColorfulButtonState extends State<ColorfulButton> {
+  bool isPressed = false;
+  Color mainColor, secondColor;
+  IconData iconData;
+
+  _ColorfulButtonState(this.mainColor, this.secondColor, this.iconData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: pi / 4,
+      child: GestureDetector(
+        onTapDown: (details) {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        onTapUp: (details) {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        onTapCancel: () {
+          setState(() {
+            isPressed = !isPressed;
+          });
+        },
+        child: Material(
+          borderRadius: BorderRadius.circular(15),
+          elevation: (isPressed) ? 5 : 10,
+          shadowColor: (isPressed) ? secondColor : mainColor,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Stack(
+              children: <Widget>[
+                SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(15),
+                    color: (isPressed) ? secondColor : mainColor,
+                    child: Transform.rotate(
+                      angle: -pi / 4,
+                      child: Icon(
+                        iconData,
+                        // Icons.adb,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(30, 30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-30, 30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(30, -30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-30, -30),
+                  child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
