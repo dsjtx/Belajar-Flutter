7f87b4daf501ad5118d341bcc18f6cc9f1673ab9
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-08-07 17:57:02 +0700

    ClipPath

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:characters/characters.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:stagehand/stagehand.dart';
 
 // void main() => runApp(MyApp());
 void main() => runApp(MyApp());
@@ -10,36 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  AudioPlayer audioPlayer;
-  String durasi = "00:00:00";
-
-  _MyAppState() {
-    audioPlayer = AudioPlayer();
-    audioPlayer.onAudioPositionChanged.listen((duration) {
-      setState(() {
-        durasi = duration.toString();
-      });
-    });
-    audioPlayer.setReleaseMode(ReleaseMode.LOOP);
-  }
-
-  void playSound(String url) async {
-    await audioPlayer.seek(Duration(seconds: 30));
-    await audioPlayer.play(url);
-  }
-
-  void pauseSound() async {
-    await audioPlayer.pause();
-  }
-
-  void stopSound() async {
-    await audioPlayer.stop();
-  }
-
-  void resumeSound() async {
-    audioPlayer.resume();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -48,42 +22,31 @@ class _MyAppState extends State<MyApp> {
           title: Text('Playing Music'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  playSound(
-                      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Nutmeg/Camelot_Calling/Nutmeg_-_06_-_Notes_on_a_Fesival_Take_me_home_Emily_Im_Tired.mp3");
-                },
-                child: Text('Play'),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  pauseSound();
-                },
-                child: Text('Pause'),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  stopSound();
-                },
-                child: Text('Stop'),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  resumeSound();
-                },
-                child: Text('Resume'),
-              ),
-              Text(
-                durasi,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-              )
-            ],
-          ),
-        ),
+            child: ClipPath(
+          clipper: MyClipper(),
+          child: Image(
+              image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg")),
+        )),
       ),
     );
   }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.75, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
