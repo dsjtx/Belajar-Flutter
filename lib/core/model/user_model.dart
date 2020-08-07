0648f7968232413b4db3b5e204e98fed0d3f024c
Daniel Simanjuntak <daniel.smnjuntak@gmail.com>
2020-08-07 22:15:23 +0700

    HTTP Request / Koneksi ke API (Kumpulan Data)

--- lib/core/model/user_model.dart
+++ lib/core/model/user_model.dart
@@ -1,28 +1,2 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
-class User{
-  String id;
-  String name;
-  String email;
 
-  User({this.id, this.name, this.email});
-
-  factory User.createUser(Map<String, dynamic> object){
-    return User(
-      id: object['id'].toString(),
-      name: object['first_name'] + object['last_name'],
-      email: object['email'],
-    );
-  }
-
-  static Future<User> connectToAPI(String id) async {
-    String apiURL = "https://reqres.in/api/users/" + id;
-
-    var apiResult = await http.get(apiURL);
-    var jsonObject = json.decode(apiResult.body);
-    var userData = (jsonObject as Map<String, dynamic>)['data'];
-
-    return User.createUser(userData);
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'dart:ui';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:characters/characters.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:hello_world/core/model/post_result.dart';
 import 'package:stagehand/stagehand.dart';
-
 import 'core/model/user_model.dart';
 
 // void main() => runApp(MyApp());
@@ -17,6 +17,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String text = "No Data Found";
   PostResult postResult = null;
   User user = null;
 
@@ -42,7 +43,7 @@ class _MyAppState extends State<MyApp> {
             //     : "Tidak ada data"),
             Text((user != null)
                 ? user.id + " | " + user.name + " | " + user.email
-                : "Tidak ada data"),
+                : text),
             RaisedButton(
               onPressed: () {
                 // PostResult.connectToAPI("Badu", "Dokter").then((value) {
@@ -52,8 +53,16 @@ class _MyAppState extends State<MyApp> {
                 //   });
                 // });
 
-                User.connectToAPI("5").then((value) {
-                  user = value;
+                // User.connectToAPI("5").then((value) {
+                //   user = value;
+                //   setState(() {});
+                // });
+
+                User.getUsers("2").then((value) {
+                  text = "";
+                  for (var i = 0; i < value.length; i++) {
+                    text = text + "[ " + value[i].name + " ]";
+                  }
                   setState(() {});
                 });
               },
@@ -64,5 +73,71 @@ class _MyAppState extends State<MyApp> {
         )),
       ),
     );
+  }
+}
+
+class User {
+  String id;
+  String name;
+  String email;
+
+  User({this.id, this.name, this.email});
+
+  factory User.createUser(Map<String, dynamic> object) {
+    return User(
+      id: object['id'].toString(),
+      name: object['first_name'] + " " + object['last_name'],
+      email: object['email'],
+    );
+  }
+
+  // static Future<User> connectToAPI(String id) async {
+  //   String apiURL = "https://reqres.in/api/users/" + id;
+
+  //   var apiResult = await http.get(apiURL);
+  //   var jsonObject = json.decode(apiResult.body);
+  //   var userData = (jsonObject as Map<String, dynamic>)['data'];
+
+  //   return User.createUser(userData);
+  // }
+
+  static Future<List<User>> getUsers(String page) async {
+    String apiURL = "https://reqres.in/api/users?page=" + page;
+
+    var apiResult = await http.get(apiURL);
+    var jsonObject = json.decode(apiResult.body);
+    List<dynamic> listUser = (jsonObject as Map<String, dynamic>)['data'];
+
+    List<User> users = [];
+    for (var i = 0; i < listUser.length; i++) {
+      users.add(User.createUser(listUser[i]));
+    }
+    return users;
+  }
+}
+
+class PostResult {
+  String id;
+  String name;
+  String job;
+  String created;
+
+  PostResult({this.id, this.name, this.job, this.created});
+
+  factory PostResult.createPostResult(Map<String, dynamic> object) {
+    return PostResult(
+        id: object['id'],
+        name: object['name'],
+        job: object['job'],
+        created: object['createdAt']);
+  }
+
+  static Future<PostResult> connectToAPI(String name, String job) async {
+    String apiURL = "https://reqres.in/api/users";
+
+    var apiResult = await http.post(apiURL, body: {"name": name, "job": job});
+    var jsonObject = json.decode(apiResult.body);
+
+    return PostResult.createPostResult(jsonObject);
   }
 }
